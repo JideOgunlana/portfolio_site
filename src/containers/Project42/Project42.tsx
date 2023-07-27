67dffe22f7c2532b8f7aa2d7f413226c176c29e1
Babajide Ogunlana <bogunlan@3-D-5.42heilbronn.de>
2023-07-27 22:13:06 +0200

    refactored project42 component

--- src/containers/Project42/Project42.tsx
+++ src/containers/Project42/Project42.tsx
@@ -17,10 +17,47 @@ import cpp from "../../assets/images/cpp-file.png";
 import containers from "../../assets/images/container.png";
 import chat from "../../assets/images/bubble-chat.png";
 import pingPong from "../../assets/images/ping-pong.png";
+import code from "../../assets/images/code.png";
 
 
 type Project42Props = {
     setBackToProjectsPage: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+type ProjectCircleProps = {
+    projectTitle?: string; // optional, only for the first circle in each category to show its
+    projectImg: string;
+    imgAltText: string;
+    myClass: string;
+}
+
+type ProjectSummaryProps = {
+    summaryContent: string;
+}
+
+const ProjectCircle: React.FC<ProjectCircleProps> = ({projectTitle, projectImg, imgAltText, myClass}) => {
+    return (
+        <div className={myClass}>
+            <img src={projectImg} alt={imgAltText} />
+            {projectTitle}
+        </div>
+    );
+}
+
+const ProjectSummary: React.FC<ProjectSummaryProps> = ({summaryContent}) => {
+    return (
+        <div className="project-summary" >
+            <div className="tool-tip">
+                <div>
+                    <img src={code} alt="fragment tag" />
+                    <h3>summary</h3>
+                </div>
+                <p>
+                    {summaryContent}
+                </p>
+            </div>
+        </div> 
+    );
 }
 
 
@@ -36,107 +73,99 @@ const Project42: React.FC<Project42Props> = ({setBackToProjectsPage}) => {
                 <div className="titleLine"></div>
             </div>
             <div className="levels level-0">
-                <div className="project42 completed">
-                    <img src={library} alt="library" />
-                    Libft
+                <div>
+                    <ProjectCircle projectTitle="libft" projectImg={library} imgAltText="library"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is libft"/>
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-1">
-                <div className="project42 completed">
-                    <img src={menu} alt="lines" />
-                    gnl
+                <div>
+                    <ProjectCircle projectTitle="gnl" projectImg={menu} imgAltText="lines"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is gnl" />
                 </div>
-                <div className="project42 completed">
-                    <img src={terminal} alt="terminal" />
-                    ft_printf
+                <div>
+                    <ProjectCircle projectTitle="ft_printf" projectImg={terminal} imgAltText="terminal"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is ft_printf" />
                 </div>
-                <div className="project42 completed">
-                    <img src={os} alt="operating system" />
-                    b2broot
+                <div>
+                    <ProjectCircle projectTitle="b2broot" projectImg={os} imgAltText="operating system"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is Born2beroot" />
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-2">
-                <div className="project42 completed">
-                    <img src={algorithm} alt="algorithm" />
-                    push_swap
+                <div>
+                    <ProjectCircle projectTitle="push_swap" projectImg={algorithm} imgAltText="algorithm"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is push_swap" />
                 </div>
-                <div className="project42 completed">
-                    <img src={joystick} alt="joystick" />
-                    so_long
+                <div>
+                    <ProjectCircle projectTitle="so_long" projectImg={joystick} imgAltText="joystick"  myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is so_long" />
                 </div>
-                <div className="project42 completed">
-                    <img src={ipc} alt="inter process communication" />
-                    minitalk
+                <div>
+                    <ProjectCircle projectTitle="minitalk" projectImg={ipc} imgAltText="Inter Process Communication" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is minitalk" />
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-3">
-                <div className="project42 completed">
-                    <img src={bash} alt="bash shell" />
-                    minishell
+                <div>
+                    <ProjectCircle projectTitle="minishell" projectImg={bash} imgAltText="bash shell" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is minishell" />
                 </div>
-                <div className="project42 completed">
-                    <img src={philo} alt="philosopher" />
-                    philosophers
+                <div>
+                    <ProjectCircle projectTitle="philosophers" projectImg={philo} imgAltText="philosopher" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is philosophers" />
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-4">
-                <div className="project42 completed">
-                    <img src={networks} alt="networks" />
-                    netpractice
+                <div>
+                    <ProjectCircle projectTitle="netpractice" projectImg={networks} imgAltText="networks" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is netpractice" />
                 </div>
-                <div className="project42 completed">
-                    <img src={cube} alt="cube" />
-                    Cub3D
+                <div>
+                    <ProjectCircle projectTitle="Cub3D" projectImg={cube} imgAltText="cube" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is Cub3D" />
                 </div>
-                <div className="project42 completed">
-                    <img src={cpp} alt="cpp file" />
-                    <div>
-                        cpp_modules
-                        <br/>
-                        0 - 4
-                    </div>
+                <div>
+                    <ProjectCircle projectTitle="cpp_modules" projectImg={cpp} imgAltText="cpp file" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is CPP 0-4" />
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-5">
-                <div className="project42 completed">
-                    <img src={containers} alt="containers" />
-                    inception
+                <div>
+                    <ProjectCircle projectTitle="inception" projectImg={containers} imgAltText="containers" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is Inception" />
                 </div>
-                <div className="project42 in-progress">
-                    <img src={chat} alt="chat bubble" />
-                    ft_irc
+                <div>
+                    <ProjectCircle projectTitle="ft_irc" projectImg={chat} imgAltText="chat bubble" myClass={"project42 in-progress"}/>
+                    <ProjectSummary summaryContent="This is ft_irc" />
                 </div>
-                <div className="project42 completed">
-                <img src={cpp} alt="cpp file" />
-                    <div>
-                        cpp_modules
-                        <br/>
-                        5 - 9
-                    </div>
+                <div>
+                    <ProjectCircle projectTitle="cpp_modules" projectImg={cpp} imgAltText="cpp file" myClass={"project42 completed"}/>
+                    <ProjectSummary summaryContent="This is CPP 5-9" />
                 </div>
             </div>
             <div className="down-arrow">
                 <img src={downArrow} alt="down arrow" />
             </div>
             <div className="levels level-6">
-                <div className="project42 not-started">
-                    <img src={pingPong} alt="2D ping pong game" />
-                    transcendence
+                <div>
+                    <ProjectCircle projectTitle="transcendence" projectImg={pingPong} imgAltText="2D ping pong game" myClass={"project42 not-started"}/>
+                    <ProjectSummary summaryContent="This is transcendence" />
                 </div>
             </div>
         </>
